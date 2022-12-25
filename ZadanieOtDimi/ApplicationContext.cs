6761321881fab357a6a764defb30cad3e6f23f2e
DQuest <DQuest1996@gmail.com>
2022-12-25 16:12:12 +0500

    refactoring

--- ZadanieOtDimi/ApplicationContext.cs
+++ ZadanieOtDimi/ApplicationContext.cs
@@ -7,6 +7,7 @@ public class ApplicationContext : DbContext
     public DbSet<City> Cities => Set<City>();
     public DbSet<User> Users => Set<User>();
     public DbSet<Animal> Animals => Set<Animal>();
+
     public ApplicationContext() => Database.EnsureCreated();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- ZadanieOtDimi/Program.cs
+++ ZadanieOtDimi/Program.cs
@@ -1,142 +1,175 @@
 ﻿namespace ZadanieOtDimi;
+
 using System.Threading;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.Write("Из какой таблицы вывести данные?\n" +
-                          "1. Animal\n" +
-                          "2. User\n" +
-                          "3. City\n" +
-                          "Введите цифру для вывода соответствующей таблицы: ");
-        using ApplicationContext db = new ApplicationContext();
+        using var db = new ApplicationContext();
         while (true)
         {
-            var str = Console.ReadLine();
-            if (string.IsNullOrEmpty(str)){}
+            GetRequestText();
+            var value = Console.ReadLine();
+            if (string.IsNullOrEmpty(value))
+            {
+                InvalidCommandAction();
+            }
             else
             {
-                switch (str.ToLower())
+                switch (value.ToLower())
                 {
-                    
                     case "1":
+                    {
                         WorkWithAnimal(db);
-                        return;
-                    case "2":
-                        WorkWithUser(db);
-                        return;
-                    case "3":
-                        WorkWithCity(db);
-                        return;
-                    case "r":
-                        Console.Clear();
-                        Thread.Sleep(1200);
-                        Console.WriteLine("Из какой таблицы вывести данные?\n" +
-                                          "1. Animal\n" +
-                                          "2. User\n" +
-                                          "3. City\n" +
-                                          "Введите цифру для вывода соответствующей таблицы");
                         break;
+                    }
+                    case "2":
+                    {
+                        WorkWithUser(db);
+                        break;
+                    }
+                    case "3":
+                    {
+                        WorkWithCity(db);
+                        break;
+                    }
+                    case "r":
+                    {
+                        InvalidCommandAction("Сброс...");
+                        break;
+                    }
                     case "e":
                         return;
                     default:
-                        Console.Clear();
-                        Console.WriteLine("Неверная команда");
-                        Thread.Sleep(2600);
-                        Console.Clear();
-                        Console.WriteLine("Из какой таблицы вывести данные?\n" +
-                                          "1. Animal\n" +
-                                          "2. User\n" +
-                                          "3. City\n" +
-                                          "Введите цифру для вывода соответствующей таблицы");
+                    {
+                        InvalidCommandAction();
                         break;
+                    }
                 }
             }
         }
     }
 
+    #region City
+
     private static void WorkWithCity(ApplicationContext db)
     {
-        Console.Clear();
-        
-        PrintCity(db);
-        Console.Write("Выберите вариант сортировки:\n" +
-                      "1. По названию города\n" +
-                      "Введите значение:");
-        
-        var sortNumber = Console.ReadLine();
-        switch (sortNumber)
+        while (true)
         {
-            case "1":
-                Console.Clear();
-                var citySortWithNameCity = db.Cities.OrderBy(c => c.NameCity);
-                Console.WriteLine("Id | City");
-                foreach (City c in citySortWithNameCity)
+            Console.Clear();
+            PrintCity(db);
+
+            Console.Write("\nВыберите вариант сортировки:\n" +
+                          "1. По названию города\n" +
+                          "Введите значение: ");
+
+            var sortNumber = Console.ReadLine();
+            switch (sortNumber)
+            {
+                case "1":
                 {
-                    Console.WriteLine("{0, 2} | {1, 2}", c.Id, c.NameCity);
+                    Console.Clear();
+                    Console.WriteLine("| Id | City |");
+
+                    foreach (var city in db.Cities.OrderBy(c => c.NameCity))
+                    {
+                        Console.WriteLine($"| {city.Id} | {city.NameCity} |");
+                    }
+
+                    PressAnyKey();
+                    return;
                 }
-                break;
-            default:
-                Console.Clear();
-                Console.WriteLine("Такой сортировки нет");
-                Thread.Sleep(2700);
-                Console.Clear();
-                Console.Write("Выберите вариант сортировки:\n" +
-                              "1. По названию города\n" + 
-                              "Введите значение:");
-                break;
+                default:
+                {
+                    InvalidCommandAction("Такой сортировки нет");
+                    break;
+                }
+            }
         }
     }
+
+    private static void PrintCity(ApplicationContext db)
+    {
+        Console.WriteLine("| Id | City |");
+        foreach (var city in db.Cities.ToList())
+        {
+            Console.WriteLine($"| {city.Id} | {city.NameCity} |");
+        }
+    }
+
+    #endregion
+
+
+    #region User
 
     private static void WorkWithUser(ApplicationContext db)
     {
-        Console.Clear();
-        PrintUser(db);
-        Console.Write("Выберите вариант сортировки:\n" +
-                      "1. По имени\n" +
-                      "2. По возрасту\n" +
-                      "Введите значение:");
-        var sortNumber = Console.ReadLine();
-        switch (sortNumber)
+        while (true)
         {
-            case "1":
-                Console.Clear();
-                var userSortWithName = db.Users.OrderBy(a => a.Name);
-                Console.WriteLine(" Id |    Name   | Age");
-                foreach (User u in userSortWithName)
-                {
-                    Console.WriteLine("{0,3} | {1,9} | {2,2}", u.Id, u.Name, u.Age);
-                }
+            Console.Clear();
+            PrintUser(db);
 
-                break;
-            case "2":
-                Console.Clear();
-                var userSortWithAge = db.Users.OrderBy(a => a.Age);
-                Console.WriteLine(" Id |    Name   | Age");
-                foreach (User u in userSortWithAge)
+            Console.Write("\nВыберите вариант сортировки:\n" +
+                          "1. По имени\n" +
+                          "2. По возрасту\n" +
+                          "E. Выход\n" +
+                          "Введите значение: ");
+
+            var sortNumber = Console.ReadLine();
+            switch (sortNumber.ToLower())
+            {
+                case "1":
                 {
-                    Console.WriteLine("{0,3} | {1,9} | {2,2}", u.Id, u.Name, u.Age);
+                    var userSortedByName = db.Users.OrderBy(a => a.Name);
+                    PrintSortedUser(userSortedByName);
+                    PressAnyKey();
+                    break;
                 }
-                break;
-            case "e":
-                return;
-            default:
-                Console.Clear();
-                Console.WriteLine("Такой сортировки нет");
-                Thread.Sleep(2700);
-                Console.Clear();
-                Console.Write("Выберите вариант сортировки:\n" +
-                              "1. По имени\n" +
-                              "2. По возрасту\n" +
-                              "Введите значение:");
-                break;
+                case "2":
+                {
+                    var userSortedByAge = db.Users.OrderBy(a => a.Age);
+                    PrintSortedUser(userSortedByAge);
+                    PressAnyKey();
+                    break;
+                }
+                case "e":
+                {
+                    return;
+                }
+                default:
+                {
+                    InvalidCommandAction("Такой сортировки нет");
+                    break;
+                }
+            }
         }
     }
 
-    private static void WorkWithAnimal(ApplicationContext db)
+    private static void PrintUser(ApplicationContext db)
+    {
+        Console.WriteLine("| Id |    Name   | Age |");
+        foreach (var user in db.Users.ToList())
+        {
+            Console.WriteLine($"| {user.Id} | {user.Name} | {user.Age} |");
+        }
+    }
+
+    private static void PrintSortedUser(IOrderedQueryable<User> sortedUser)
     {
         Console.Clear();
+        Console.WriteLine("| Id |    Name   | Age |");
+        foreach (var user in sortedUser)
+        {
+            Console.WriteLine($"| {user.Id} | {user.Name} | {user.Age} |");
+        }
+    }
+
+    #endregion
+
+
+    private static void WorkWithAnimal(ApplicationContext db)
+    {
         PrintAnimal(db);
         Thread.Sleep(3000);
         Console.Write("Выберите вариант сортировки:\n" +
@@ -150,10 +183,10 @@ internal class Program
             case "1":
                 Console.Clear();
                 var animalSortWithName = db.Animals.OrderBy(a => a.Name);
-                Console.WriteLine("Id|   Name  | Age |    Breed");
-                foreach (Animal a in animalSortWithName)
+                Console.WriteLine("|   Id   |   Name  | Age |    Breed    |");
+                foreach (var a in animalSortWithName)
                 {
-                    Console.WriteLine("{0, 1} | {1, 7} |  {2, 1}  | {3, 1}", a.Id, a.Name, a.Age, a.Breed);
+                    Console.WriteLine("| {0, 1} | {1, 7} |  {2, 1}  | {3, 1} |", a.Id, a.Name, a.Age, a.Breed);
                 }
 
                 break;
@@ -161,19 +194,21 @@ internal class Program
                 Console.Clear();
                 var animalSortWithAge = db.Animals.OrderBy(a => a.Age);
                 Console.WriteLine("Id|   Name  | Age |    Breed");
-                foreach (Animal a in animalSortWithAge)
+                foreach (var a in animalSortWithAge)
                 {
                     Console.WriteLine("{0, 1} | {1, 7} |  {2, 1}  | {3, 1}", a.Id, a.Name, a.Age, a.Breed);
                 }
+
                 break;
             case "3":
                 Console.Clear();
                 var animalSortWithBreed = db.Animals.OrderBy(a => a.Breed);
                 Console.WriteLine("Id|   Name  | Age |    Breed");
-                foreach (Animal a in animalSortWithBreed)
+                foreach (var a in animalSortWithBreed)
                 {
                     Console.WriteLine("{0, 1} | {1, 7} |  {2, 1}  | {3, 1}", a.Id, a.Name, a.Age, a.Breed);
                 }
+
                 break;
             default:
                 Console.Clear();
@@ -186,51 +221,42 @@ internal class Program
                               "3. По породе\n" +
                               "Введите значение:");
                 break;
-                
         }
     }
-    
-    private static void PrintCity(ApplicationContext db)
-    {
-        var cities = db.Cities.ToList();
-        Console.WriteLine("Id | City");
-        foreach (City c in cities)
-        {
-            Console.WriteLine("{0, 2} | {1, 2}", c.Id, c.NameCity );
-        }
-    }
-    
-    private static void PrintUser(ApplicationContext db)
-    {
-        var users = db.Users.ToList();
-        Console.WriteLine(" Id |    Name   | Age");
-        foreach (User u in users)
-        {
-            Console.WriteLine("{0,3} | {1,9} | {2,2}", u.Id, u.Name, u.Age);
-        }
-    }
-    
+
     private static void PrintAnimal(ApplicationContext db)
     {
-        var animals = db.Animals.ToList();
-        Console.WriteLine("Id|   Name  | Age |    Breed");
-        foreach (Animal a in animals)
+        Console.WriteLine("| Id |   Name  | Age |    Breed    |");
+        foreach (var animal in db.Animals.ToList())
         {
-            Console.WriteLine("{0, 1} | {1, 7} |  {2, 1}  | {3, 1}", a.Id, a.Name, a.Age, a.Breed);
+            Console.WriteLine($"| {animal.Id} | {animal.Name} |  {animal.Age}  | {animal.Breed} |");
         }
     }
+
+    #region Common
+
+    private static void GetRequestText()
+    {
+        Console.Clear();
+        Console.Write("Из какой таблицы вывести данные?\n" +
+                      "1. Animal\n" +
+                      "2. User\n" +
+                      "3. City\n" +
+                      "Введите цифру для вывода соответствующей таблицы: ");
+    }
+
+    private static void InvalidCommandAction(string message = "Неверная команда")
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+        Thread.Sleep(2000);
+    }
+
+    private static void PressAnyKey()
+    {
+        Console.WriteLine("\nНажмите любую кнопку для продолжения...");
+        Console.ReadKey();
+    }
+
+    #endregion
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
